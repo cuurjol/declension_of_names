b2c52cc955f779ebcb1b14a8889f68ead3088ceb
Kirill Ilyin <cuurjol@gmail.com>
2019-10-13 18:56:51 +0300

    Add validation for russian letters

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,7 @@ class Person < ApplicationRecord
   has_one :declension_name, dependent: :destroy
 
   validates :first_name, :full_name, presence: true
+
+  validates :first_name, :last_name, :middle_name, format: { with: /\A\p{Cyrillic}+\z/,
+                                                             message: 'только русские буквы' }
 end
